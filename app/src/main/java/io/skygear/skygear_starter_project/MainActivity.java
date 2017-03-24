253ab36929033cfffd23bdb216da8c9e7fc10812
david90 <david.ng@oursky.com>
2017-03-24 15:20:08 +0800

    Implement reply to ping and send custom data content

--- app/src/main/java/io/skygear/skygear_starter_project/MainActivity.java
+++ app/src/main/java/io/skygear/skygear_starter_project/MainActivity.java
@@ -4,17 +4,29 @@ import android.app.ProgressDialog;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
+
+import java.sql.Time;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Map;
 
 import io.skygear.skygear.AuthResponseHandler;
 import io.skygear.skygear.Configuration;
 import io.skygear.skygear.Container;
+import io.skygear.skygear.Database;
 import io.skygear.skygear.LogoutResponseHandler;
+import io.skygear.skygear.Pubsub;
+import io.skygear.skygear.Record;
+import io.skygear.skygear.RecordSaveResponseHandler;
 import io.skygear.skygear.User;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
     private Button signupButton;
     private Button loginButton;
     private Button logoutButton;
+    private Button sendDataButton;
+    private EditText dataContentInput;
 
     private Container skygear;
 
@@ -49,6 +63,8 @@ public class MainActivity extends AppCompatActivity {
         this.signupButton = (Button) findViewById(R.id.signup_button);
         this.loginButton = (Button) findViewById(R.id.login_button);
         this.logoutButton = (Button) findViewById(R.id.logout_button);
+        this.sendDataButton = (Button) findViewById(R.id.send_data);
+        this.dataContentInput = (EditText) findViewById(R.id.data_content_input);
 
         this.skygear = Container.defaultContainer(this);
     }
@@ -72,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
         if (currentUser != null) {
             accessToken = currentUser.getAccessToken();
             userId = currentUser.getId();
+            subscribeToPing();
         } else {
 
         }
@@ -82,6 +99,11 @@ public class MainActivity extends AppCompatActivity {
         this.signupButton.setEnabled(currentUser == null);
         this.loginButton.setEnabled(currentUser == null);
         this.logoutButton.setEnabled(currentUser != null);
+    }
+
+    public void sendDataOnClickHandler(View view) {
+        String content = this.dataContentInput.getText().toString();
+        sendDataToServer(content);
     }
 
     public void signupOnClickHandler(View view) {
@@ -211,5 +233,105 @@ public class MainActivity extends AppCompatActivity {
                 failDialog.show();
             }
         });
+    }
+
+    public void registerReport() {
+        Pubsub pubsub = Container.defaultContainer(this).getPubsub();
+        pubsub.subscribe("update-channel", new Pubsub.Handler(){
+            @Override
+            public void handle(JSONObject data) {
+                String msg = null;
+                try {
+                    msg = data.getString("msg");
+                    Log.i("Pubsub", "Receive Update message:"+ msg);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void subscribeToPing() {
+        Pubsub pubsub = Container.defaultContainer(this).getPubsub();
+        pubsub.subscribe("ping", new Pubsub.Handler(){
+            @Override
+            public void handle(JSONObject data) {
+                String msg = null;
+                try {
+                    msg = data.getString("msg");
+                    Log.i("Pubsub", "Receive ping:"+ msg);
+                    replyToPing();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void replyToPing() throws JSONException {
+        Pubsub pubsub = Container.defaultContainer(this).getPubsub();
+        JSONObject data = new JSONObject();
+        data.put("device", skygear.getCurrentUser().getId());
+        data.put("platform", "android");
+        data.put("lastReply", DateFormat.getDateTimeInstance().format(new Date()));
+        pubsub.publish("reply", data);
+    }
+
+    public void sendDataToServer(String content) {
+        // Send record, we will have an afterSave cloud function to send event to the channel.
+        // Alternatively, we can call publishEvent on client side.
+        // To pick which of these options depends on the usecase.
+
+
+        Database publicDatabase = skygear.getPublicDatabase();
+
+        Record aReport = new Record("Report");
+        aReport.set("content", content);
+
+        RecordSaveResponseHandler handler = new RecordSaveResponseHandler(){
+            @Override
+            public void onSaveSuccess(Record[] records) {
+                Log.i(
+                        "Skygear Record Save",
+                        "Successfully saved " + records.length + " records"
+                );
+            }
+
+            @Override
+            public void onPartiallySaveSuccess(
+                    Map<String, Record> successRecords,
+                    Map<String, String> reasons
+            ) {
+                Log.i(
+                        "Skygear Record Save",
+                        "Successfully saved " + successRecords.size() + " records"
+                );
+                Log.i(
+                        "Skygear Record Save",
+                        reasons.size() + " records are fail to save"
+                );
+            }
+
+            @Override
+            public void onSaveFail(String reason) {
+                Log.i(
+                        "Skygear Record Save",
+                        "Fail to save: " + reason
+                );
+            }
+        };
+
+        publicDatabase.save(aReport, handler);
+    }
+
+    public void publishEvent(){
+        try {
+            Pubsub pubsub = Container.defaultContainer(this).getPubsub();
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("msg", "Hello World");
+            pubsub.publish("update-channel", jsonObject);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
